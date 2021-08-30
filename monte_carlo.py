db11c4b10d294f3c8be6263e5e0dcb1aa0127232
masayuki1125 <masayuki19981125@gmail.com>
2021-08-30 21:40:58 +0900

    change multi N num

--- monte_carlo.py
+++ monte_carlo.py
@@ -1,62 +1,33 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[44]:
+# In[10]:
 
 
 import numpy as np
 import ray
+import pickle
 import sys
-sys.path.append("../channel")
-from AWGN import _AWGN
-sys.path.append("../polar_code") #毎回書き換える
-from polar_code import coding
-
-cd=coding(1024)
-ch=_AWGN()
 
 
-# In[45]:
+# In[4]:
 
 
-'''
-#ray.initを一回だけ実行する
-def run_once(f):
-    def wrapper(*args, **kwargs):
-        if not wrapper.has_run:
-            wrapper.has_run = True
-            return f(*args, **kwargs)
-    wrapper.has_run = False
-    return wrapper
-
-@run_once
-def onece():
-    ray.init()
-'''
 ray.init()
 
 
-# In[47]:
+# In[ ]:
 
 
-#毎回書き換える関数その2
 @ray.remote
-def output(K,EbNodB):
+def output(dumped,EbNodB):
         '''
         #あるSNRで計算結果を出力する関数を作成
-        #main_func must input 'EbNodB' and output 1D 'codeword' and 'EST_codeword'
+        #cd.main_func must input 'EbNodB' and output 1D 'codeword' and 'EST_codeword'
         '''
 
-        #import module 
-        sys.path.append("../polar_code")
-        import polar_code
-        pc=polar_code.polar_code(K)
-        main_func=pc.polar_code
-
-        '''
-        ここから上は毎回書き換え
-        '''
-
+        #de-seriallize file
+        cd=pickle.loads(dumped)
         #seed値の設定
         np.random.seed()
 
@@ -70,7 +41,7 @@ def output(K,EbNodB):
 
         while count_err<MAX_ERR:
         #print("\r"+str(count_err),end="")
-            information,EST_information=main_func(EbNodB)
+            information,EST_information=cd.main_func(EbNodB)
             
             #calculate block error rate
             if np.any(information!=EST_information):
@@ -84,7 +55,7 @@ def output(K,EbNodB):
         return count_err,count_all,count_biterr,count_bitall
 
 
-# In[48]:
+# In[11]:
 
 
 class MC():
@@ -95,11 +66,11 @@ class MC():
         self.RX_antenna=1
         self.MAX_ERR=8
         self.EbNodB_start=-5
-        self.EbNodB_end=3
-        self.EbNodB_range=np.arange(self.EbNodB_start,self.EbNodB_end) 
+        self.EbNodB_end=1
+        self.EbNodB_range=np.arange(self.EbNodB_start,self.EbNodB_end,0.5) #0.5dBごとに測定
 
 
-# In[49]:
+# In[ ]:
 
 
 class MC(MC):
@@ -153,13 +124,12 @@ class MC(MC):
             print("\r"+"EbNodB="+str(EbNodB)+",BLER="+str(BLER[i])+",BER="+str(BER[i]),end="")
         return BLER,BER
 
-# In[46]:
+
+# In[ ]:
 
 
 #毎回書き換える関数その１
 class savetxt(coding,_AWGN,MC):
-  def __init__(self,N):
-    super().__init__(N)
 
   def savetxt(self,BLER,BER):
 
@@ -170,27 +140,21 @@ class savetxt(coding,_AWGN,MC):
         print("#RX_antenna="+str(self.RX_antenna),file=f)
         print("#modulation_symbol="+str(self.M),file=f)
         #print("#MAX_BLERR="+str(self.MAX_ERR),file=f)
-        print("#R="+str(self.R),file=f)
-        #print("#iteration number="+str(self.L_MAX),file=f)
+        print("#iteration number="+str(self.L_MAX),file=f)
         print("#EsNodB,BLER,BER",file=f) 
         for i in range(len(self.EbNodB_range)):
             print(str(self.EbNodB_range[i]),str(BLER[i]),str(BER[i]),file=f)
 
 
-# In[50]:
-
-
-K=[512,1024,2048]
-for K in K:
-    print("N=",K)
-    mc=MC()
-    BLER,BER=mc.monte_carlo(K)
-    st=savetxt(K)
-    st.savetxt(BLER,BER)
-
-
 # In[ ]:
 
 
-
+if __name__=="__main__":
+    K=[400,800,1000]
+    for K in K:
+        print("K=",K)
+        mc=MC()
+        BLER,BER=mc.monte_carlo(K)
+        st=savetxt(K)
+        st.savetxt(BLER,BER)
 
